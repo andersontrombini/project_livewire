b2a6ff46c9cb14bb60f836877b79f84ab056bf9a
Anderson <anderson@viafast.com.br>
2023-09-07 15:02:39 -0300

    validacao no campo

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -4,17 +4,36 @@ namespace App\Http\Livewire;
 
 use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowTweets extends Component
 {
-    public $message = "Viva la vida";
+    use WithPagination;
+    
+    public $content = "Viva la vida";
+
+    protected $rules  = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        $tweets = Tweet::with('user')->get();
+        $tweets = Tweet::with('user')->paginate(2);
 
         return view('livewire.show-tweets', [
             'tweets' => $tweets
         ]);
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        Tweet::create([
+            'user_id' => 1,
+            'content' => $this->content
+        ]);
+
+        $this->content = "";
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,11 +1,21 @@
 <div>
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
-    <input type="text" name="message" id="message" wire:model="message">
+    <form action="" method="post" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+        {{ $errors->has('content') ? $errors->first('content') : '' }}
+        <button type="submit">Criar Tweet</button>
+    </form>
 
     <hr>
 
     @foreach ($tweets as $tweet)
         {{ $tweet->user->name }} - {{ $tweet->content }} <br><br>
     @endforeach
+
+    <hr>
+
+    <div>
+        {{ $tweets->links()}}
+    </div>
 </div>
